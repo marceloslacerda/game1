119c019954ae447243bc8c8d269815db29e4e965
Marcelo Lacerda <marceloslacerda@gmail.com>
2013-01-05 01:58:29 -0200

    Licensed the code under gpl v3.

--- src/main/scala/com/botequim/witchcraft/swing/canvas.scala
+++ src/main/scala/com/botequim/witchcraft/swing/canvas.scala
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2013 Marcelo de Sena Lacerda
+ *
+ * This file is part of Witchcraft.
+ * 
+ * Witchcraft is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Witchcraft is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Witchcraft.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.botequim.witchcraft.swing
 
 import java.awt.{Graphics2D, Color, Rectangle}
